c284b19cb1d4c677c7147f1dc7175bfc8c497f5c
JulienKEIFF <j.keiff@hotmail.fr>
2020-12-13 13:22:52 +0100

    add cart

---
+++ src/components/cartItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="cart-item">
+    
+    <div class="info">
+      <img :src="getImg(data.img)" class="item-img" >
+
+      <div class="item-name">
+        {{data.name}}
+      </div>
+
+      <div class="item-quantity">
+        X{{data.qte}}
+      </div>
+
+      <div class="item-price">
+        {{data.price}} €
+      </div>
+    </div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cart-item",
+  props:{
+    data: Object
+  },
+  methods:{
+    getImg: function(item){
+      return require(`../assets/img/${item}`)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.cart-item{
+  position: relative;
+  height: 5vh;
+  margin: 1% 0;
+  width: 100%;
+  .info{
+    height: 100%;
+    display: flex;
+    align-items: center;
+    .item-img{
+      height: 100%;
+      width: 15%;
+      object-fit: cover;
+      margin-right: 2%;
+    }
+    .item-name{
+      width: 55%;
+    }
+    .item-quantity{
+      width: 7%
+    }
+    .item-price{
+      width: 20%;
+      text-align: right;
+    }
+  }
+}
+</style>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="navbar">
     
+    <transition name="fade-delay">
+      <cart v-if="cart" />
+    </transition>
+    <transition name="fade">
+      <div id="bg" v-if="cart" @click="cart = !cart"></div>
+    </transition>
+
     <div id="info">
       <input type="text" name="search" id="search" placeholder="Rechercher un produit"> <label for="search"></label>
-      <p class="click" v-for="item in info" :key="item">{{ item }}</p>
+      <p class="click" v-for="(item, i) in info" :key="item" v-on:click="checkInteract(i)">{{ item }}</p>
     </div>
   
     <div id="nav">
@@ -13,103 +20,138 @@
 </template>
 
 <script>
+import cart from "./cart"
 export default {
   name: "navbar",
+  components:{
+    cart
+  },
   data(){
     return{
-      info: ["Retrait des produit sous 2H", "Trouver un point de retrait", "Suivis de commande", "S'identifier", "Panier"],
-      page: ["LOGO", "Espace Pare-douche", "Espace Cloison et parois", "Espace cheminée"]
+      info: ["Retrait des produit sous 2H", "Trouver un point de retrait", "Suivis de commande", "Panier", "S'identifier"],
+      page: ["LOGO", "Espace Pare-douche", "Espace Cloison et parois", "Espace cheminée"],
+      cart: false
     }
   },
   methods: {
     checkPath: function(value){
       if(value == 0 && this.$route.path !== "/") { this.$router.push({ path: '/' }) };
       if(value == 1) { this.$router.push({ path: '/product' }) };
+    },
+    checkInteract: function(value){
+      if(value == 3){this.cart = !this.cart}
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-  #navbar{
+#navbar{
+  width: 100vw;
+  height: 10vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 10;
+  background-color: #eee;
+  #bg{
+    position: absolute;
+    height: 100vh;
     width: 100vw;
-    height: 10vh;
-    position: fixed;
-    top: 0;
     left: 0;
+    background: rgba(0, 0, 0, 0.198);
     z-index: 10;
-    background-color: #eee;
-    #info{
-      height: 2vh;
-      padding: 10px 0px;
-      margin-right: 50px;
-      width: 98.5%;
-      display: flex;
-      flex-direction: row;
-      justify-content: flex-end;
-      #search{
-        position: absolute;
-        left: 5%;
-        border-radius: 3% 3% 3% 3% / 50% 50% 50% 50% ;
-        border: none;
-        width: 20vw;
-        height: 20%;
-        &::placeholder{
-          padding-left: 3%;
-        }
-      }
-      .click{
-        display: flex;
-        align-items: center;
-        margin: 0;
-        padding-right: 0.5%;
-        padding-left: 0.5%;
-        border-left: 1px solid black;
-        font-size: 90%;
-        cursor: pointer;
-        &:first-of-type{
-          border: none;
-        }
-        &:hover{
-          color: #6b6b6b;
-        }
+  }
+  #info{
+    height: 2vh;
+    padding: 10px 0px;
+    margin-right: 50px;
+    width: 98.5%;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+    #search{
+      position: absolute;
+      left: 5%;
+      border-radius: 3% 3% 3% 3% / 50% 50% 50% 50% ;
+      border: none;
+      width: 20vw;
+      height: 20%;
+      &::placeholder{
+        padding-left: 3%;
       }
     }
-    #nav{
-      width: 100%;
-      height: 7vh;
-      background: white;
+    .click{
       display: flex;
-      flex-direction: row;
-      box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.15);
-      .page{
-        justify-content: center;
-        position: relative;
-        display: flex;
-        align-items: center;
-        margin: 0;
-        padding: 0 1%;
-        margin: 0 0.5%;
-        font-size: 2.5vh;
-        cursor: pointer;
-        &:hover>.bar{
-          transition-duration: 0.2s;
-          width: 100%;
-          opacity: 1;
-        }
-        &:first-of-type{
-          margin-left: 5%;
-        }
-        .bar{
-          transition-duration: 0.2s;
-          background: black;
-          position: absolute;
-          width: 0%;
-          opacity: 0;
-          height: 4%;
-          bottom: 20%;
-        }
+      align-items: center;
+      margin: 0;
+      padding-right: 0.5%;
+      padding-left: 0.5%;
+      border-left: 1px solid black;
+      font-size: 90%;
+      cursor: pointer;
+      &:first-of-type{
+        border: none;
+      }
+      &:hover{
+        color: #6b6b6b;
       }
     }
   }
+  #nav{
+    width: 100%;
+    height: 7vh;
+    background: white;
+    display: flex;
+    flex-direction: row;
+    box-shadow: 10px 10px 10px rgba(0, 0, 0, 0.15);
+    .page{
+      justify-content: center;
+      position: relative;
+      display: flex;
+      align-items: center;
+      margin: 0;
+      padding: 0 1%;
+      margin: 0 0.5%;
+      font-size: 2.5vh;
+      cursor: pointer;
+      &:hover>.bar{
+        transition-duration: 0.2s;
+        width: 100%;
+        opacity: 1;
+      }
+      &:first-of-type{
+        margin-left: 5%;
+      }
+      .bar{
+        transition-duration: 0.2s;
+        background: black;
+        position: absolute;
+        width: 0%;
+        opacity: 0;
+        height: 4%;
+        bottom: 20%;
+      }
+    }
+  }
+}
+
+.fade-delay-enter-active{
+  transition: opacity .5s;
+  transition-delay: 0.3s;
+}
+.fade-delay-leave-active {
+  transition: opacity .5s;
+  transition-delay: 0;
+}
+.fade-delay-enter, .fade-delay-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
