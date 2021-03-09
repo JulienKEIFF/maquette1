2bc4b1af1e09b89b430e2e88fcf7266af7f522b8
JulienKEIFF <j.keiff@hotmail.fr>
2021-03-09 11:38:59 +0100

    Add customer page

---
+++ src/components/admin/customer.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <div class="name">{{name}}</div>
+    <div class="id">{{id}}</div>
+
+    <div class="infos">
+      <button>Voir le profile</button>
+      <button>Voir les commandes</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'customer-item',
+  props: ['name', 'id'],
+}
+</script>
+
+<style scoped lang="scss">
+.container{
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  background: #ffffff;
+  height: 5vh;
+  border-radius: 10vw / 10vw;
+  margin-bottom: 1%;
+  div{
+    display: inline-block;
+    margin: 0 1%; 
+  }
+  .infos{
+    position: absolute;
+    right: 0;
+    width: 18%;
+    button{
+      margin: 0 1%;
+      border-radius: 20vw / 20vw;
+      padding: 2% 5%;
+      border: none;
+      transition: 0.2s;
+      &:focus{
+        outline: none;
+      }
+      &:hover{
+        background: #4CAF50;
+      }
+    }
+  }
+}
+</style>
--- src/views/Customer.vue
+++ src/views/Customer.vue
@@ -2,17 +2,42 @@
   <div id="admin">
     <navbarVert /> 
     <div id="content">
-      customer
+      <div class="title">Liste des clients</div>
+
+      <customer :name='item.name' :id='item.id' v-for="(item, i) in customer" :key="i" />
     </div>
   </div>
 </template>
 
 <script>
 import navbarVert from '../components/admin/navbarVertical'
+import customer from '../components/admin/customer'
 
 export default {
   name: "Admin-Customer",
-  components: {navbarVert}
+  components: {navbarVert, customer},
+  data(){
+    return{
+      customer:[
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+        {name: 'John DOE', id: 'DS546DQSe5'},
+      ]
+    }
+  }
 }
 </script>
 
@@ -21,5 +46,9 @@ export default {
 #content{
   width: 80vw;
   padding-left: 13vw;
+  padding-top: 2%;
+  .title{
+    margin-bottom: 2%;
+  }
 }
 </style>
